9dcffb00eb7a53053ea131f893f44a1c6e1f5999
Dekado <famine2@yandex.ru>
2016-04-19 00:22:20 +0400

    edits

--- sergey_mishin/lesson1/colors.js
+++ sergey_mishin/lesson1/colors.js
@@ -3,9 +3,11 @@ var colors = require('colors');
 var args = process.argv.slice(2);
 if(args.length == 0) {
     console.log('Первый параметр задает сообщение для вывода, второй - цвет. Доступные цвета: black, red, green, yellow, blue, magenta, cyan, white ,gray, grey'.green);
+    console.log('\007');
 } else {
     if(!args[0] || !args[1]) {
         console.log('Небходимо два параметра!'.red);
+        console.log('\007');
     } else {
         var arrColors = ['black', 'red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white', 'gray', 'grey'];
 
@@ -17,6 +19,7 @@ if(args.length == 0) {
             console.log(args[0][args[1]]);
         } else {
             console.log('Цвет не поддерживается!'.red);
+            console.log('\007');
         }
     }
 }
